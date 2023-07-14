84faae2f9fcae3d83e34c8c072ca1410c559e796
abhijit <abhijit@DESKTOP-LUQ9HF8>
2023-07-14 17:28:33 +0530

    second commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,11 +3,14 @@ package com.driver;
 public class Pizza {
 
     private int price;
+     private int bagp=0;
     private Boolean isVeg;
     private String bill;
 //    private Integer Tbill=0;
-    private boolean b = false;
-    private boolean b1 = false;
+    private boolean cheese = false;
+    private boolean vegT = false;
+    private boolean NvegT = false;
+     private boolean bag = false;
 
     public Pizza(Boolean isVeg){
        
@@ -26,34 +29,61 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        if(b==false){
+        if(cheese==false){
         price+=80;
-        b=true;
+        cheese=true;
         }
     }
 
     public void addExtraToppings(){
         // your code goes here
-        if(b1==false){
-        if(this.isVeg==true){
+        if(vegT==false && this.isVeg==true){
+       
         price+=70;
-        b1=true;
-        }else{
-            price+=120;
-            b1 = true;
-        }
-    }
+        vegT=true;
+        
+       }
+        
+       else if(NvegT==false&&this.isVeg==false){
+       
+        price+=120;
+        NvegT=true;
+        
+       }
+        
     }
 
     public void addTakeaway(){
         // your code goes here
         price+=20;
+        bagp+=20;
+        bag = true;
     }
 
     public String getBill(){
         // your code goes here
+       StringBuilder ab = new StringBuilder(); 
+       Integer base = isVeg==true?300:400;
+       
+       ab.append("Base Price Of The Pizza: ").append(base.toString()).append("\n");
         
-        this.bill = String.valueOf(price);
+       if(cheese==true){
+           ab.append("Extra Cheese Added: 80").append("\n");
+       }
+       if(vegT==true && isVeg==true){
+           ab.append("Extra Toppings Added: 70").append("\n"); 
+       }
+       if(NvegT==true && isVeg==false){
+           ab.append("Extra Toppings Added: 120").append("\n");
+       }
+       if(bag==true){
+           ab.append("Paperbag Added: ").append(String.valueOf(bagp)).append("\n");
+       }
+       
+       ab.append("Total Price: ").append(String.valueOf(price)).append("\n");
+       
+       this.bill = ab.toString();
+       
         return this.bill;
     }
 }
