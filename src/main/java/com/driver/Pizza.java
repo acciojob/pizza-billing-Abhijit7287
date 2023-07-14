f4543b0156eb839bd6ee3a319e310388d2817fc0
abhijit <abhijit@DESKTOP-LUQ9HF8>
2023-07-14 15:56:05 +0530

    first commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,19 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+//    private Integer Tbill=0;
+    private boolean b = false;
+    private boolean b1 = false;
 
     public Pizza(Boolean isVeg){
+       
         this.isVeg = isVeg;
-        // your code goes here
+        
+        if(this.isVeg==true){
+            price = 300;
+        }else{
+            price = 400;
+        } 
     }
 
     public int getPrice(){
@@ -17,18 +26,29 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(b==false){
+        price+=80;
+        b=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(b1==false){
+        price+=70;
+        b1=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        price+=20;
     }
 
     public String getBill(){
         // your code goes here
+        
+        this.bill = String.valueOf(price);
         return this.bill;
     }
 }
